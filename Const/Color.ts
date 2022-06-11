aeec6e92b8e42f3d79cc3ad5a9d48ffd72525d02
Jan Tichavsky <honza@resaizer.com>
2022-06-11 10:17:01 +0200

    colors added to const

---
+++ Const/Color.ts
@@ -0,0 +1,13 @@
+const COLOR = {
+    PRIMARY: { r: 204, g: 68, b: 255, a: 1 },
+    SECONDARY: { r: 68, g: 87, b: 255, a: 1 },
+    BACKGROUND: { r: 244, g: 248, b: 255, a: 1 },
+    DARK: { r: 89, g: 47, b: 116, a: 1 },
+    LIGHT: { r: 228, g: 213, b: 238, a: 1 },
+    RED: { r: 255, g: 0, b: 0, a: 1 },
+    WHITE: { r: 255, g: 255, b: 255, a: 1 },
+    GRAY: { r: 230, g: 230, b: 230, a: 1 },
+    BLACK: { r: 0, g: 0, b: 0, a: 1 },
+};
+
+export default COLOR;
