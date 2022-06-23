225c6b1ebdd894ed25bca36a1f9707b8b2e9dee9
Jan Tichavský <tichavsk@seznam.cz>
2022-06-23 23:58:32 +0200

    Add backgr dark

--- theme/dark.ts
+++ theme/dark.ts
@@ -1,42 +1,44 @@
-import { createTheme } from '@mui/material';
+import { createTheme } from "@mui/material";
 const theme = createTheme({
     typography: {
         button: {
-            textTransform: 'none',
+            textTransform: "none",
         },
         htmlFontSize: 14,
         caption: {
             fontSize: 20,
         },
-        fontFamily: ['Roboto'].join(','),
+        fontFamily: ["Roboto"].join(","),
     },
     palette: {
         primary: {
-            main: '#CC44FF',
-            light: '#ffd1f9',
+            dark: "#D200B6",
+            main: "#CC44FF",
+            light: "#ffd1f9",
         },
         secondary: {
-            main: '#4457FF',
-            light: '#DADDFF',
+            main: "#4457FF",
+            light: "#DADDFF",
         },
         error: {
-            main: '#f44336',
-            light: '#FFB3B3',
-            dark: '#FF0000',
+            main: "#f44336",
+            light: "#FFB3B3",
+            dark: "#FF0000",
         },
         success: {
-            main: '#4caf50',
-            light: '#A6FFA6',
-            dark: '#00FF00',
+            main: "#4caf50",
+            light: "#A6FFA6",
+            dark: "#00FF00",
         },
         input: {
-            selected: '#dbcaff',
-            hover: '#f5f5f5',
-            default: '#eaeaea',
-            border: '#BC3CD0',
+            selected: "#dbcaff",
+            hover: "#f5f5f5",
+            default: "#eaeaea",
+            border: "#BC3CD0",
         },
         backgr: {
-            default: 'rgb(244, 248, 255)',
+            default: "rgb(244, 248, 255)",
+            dark: "#54545A",
         },
     },
     measurements: {
