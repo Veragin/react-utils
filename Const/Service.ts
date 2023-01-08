a34c1bd1cdf564029fd12d32adb3591f22c582b4
Jan Tichavský <honza@sizefire.com>
2023-01-08 22:35:38 +0100

    add TTechTreeNames into const

---
+++ Const/Service.ts
@@ -0,0 +1,10 @@
+export const TECH_TREE_NAMES = [
+    'TEMPLATE_MAKER',
+    'TEMPLATE_RESIZE',
+    'TEMPLATE_EDIT',
+    'TEMPLATE_USER',
+    'TEMPLATE_DOWNLOAD',
+    'TEMPLATE_APPROVE',
+];
+
+export type TTechTreeName = typeof TECH_TREE_NAMES[number];
