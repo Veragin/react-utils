2b5105bcba1bdfe76e905e24522b5c3e51c06295
Jan Tichavský <honza@sizefire.com>
2023-01-08 23:09:33 +0100

    admin service api url

--- API/ServiceApi.ts
+++ API/ServiceApi.ts
@@ -1,46 +1,46 @@
-import { currentUser } from "react-utils/Wrappers/UserWrapper";
-import { TBannerSetExport, TBannerSetInfo } from "../Const/BannerSet";
+import { currentUser } from 'react-utils/Wrappers/UserWrapper';
+import { TBannerSetExport, TBannerSetInfo } from '../Const/BannerSet';
 
-import { BaseApi } from "./BaseApi";
+import { BaseApi } from './BaseApi';
 
 export class ServiceApi extends BaseApi {
     constructor() {
-        super("https://sizefire.eu/editor/v1/");
+        super('https://sizefire.eu/editor/v1/');
     }
 
     get adminUrl() {
         const user = currentUser.user;
-        if (user.roles.includes("ADMIN")) return "admin/";
-        return "";
+        if (user.roles.includes('ADMIN')) return 'admin/';
+        return '';
     }
 
     getSetsOverview = async (): Promise<TBannerSetInfo[]> => {
-        const res = await this.send("GET", `${this.adminUrl}sets`);
+        const res = await this.send('GET', `${this.adminUrl}sets`);
         return JSON.parse(res);
     };
 
     getSet = async (setId: number): Promise<TBannerSetExport> => {
-        const res = await this.send("GET", `${this.adminUrl}sets/${setId}`);
+        const res = await this.send('GET', `${this.adminUrl}sets/${setId}`);
         return JSON.parse(res);
     };
 
     createSet = async (data: TBannerSetExport): Promise<number> => {
-        const res = await this.send("POST", `${this.adminUrl}sets`, JSON.stringify(data));
+        const res = await this.send('POST', `${this.adminUrl}sets`, JSON.stringify(data));
         return JSON.parse(res);
     };
 
     updateSet = async (data: TBannerSetExport) => {
-        const res = await this.send("PUT", `${this.adminUrl}sets`, JSON.stringify(data));
+        const res = await this.send('PUT', `${this.adminUrl}sets`, JSON.stringify(data));
         return JSON.parse(res);
     };
 
     updateSetInfo = async (data: TBannerSetInfo) => {
-        const res = await this.send("PUT", `${this.adminUrl}sets/info`, JSON.stringify(data));
+        const res = await this.send('PUT', `${this.adminUrl}sets/info`, JSON.stringify(data));
         return JSON.parse(res);
     };
 
     deleteSet = async (setId: number) => {
-        const res = await this.send("DELETE", `${this.adminUrl}sets/${setId}/`);
+        const res = await this.send('DELETE', `${this.adminUrl}sets/${setId}/`);
         return JSON.parse(res);
     };
 
@@ -49,7 +49,7 @@ export class ServiceApi extends BaseApi {
      **********************************************************************************/
 
     getPublicOverview = async (): Promise<TBannerSetInfo[]> => {
-        const res = await this.send("GET", `sets/public`);
+        const res = await this.send('GET', `sets/public`);
         return JSON.parse(res);
     };
 
@@ -58,20 +58,16 @@ export class ServiceApi extends BaseApi {
      **********************************************************************************/
 
     saveImages = async (setId: number, data: TApiImageStore): Promise<TApiImageMapping> => {
-        const res = await this.send(
-            "POST",
-            `${this.adminUrl}images/${setId}/upload`,
-            JSON.stringify(data)
-        );
+        const res = await this.send('POST', `${this.adminUrl}images/${setId}/upload`, JSON.stringify(data));
         return JSON.parse(res).image;
     };
 
     getImages = async (setId: number, imgIds: number[]): Promise<TApiImage[]> => {
         const res = await this.send(
-            "POST",
+            'POST',
             `${this.adminUrl}images/${setId}/download`,
             JSON.stringify(imgIds),
-            "application/json"
+            'application/json'
         );
         return JSON.parse(res).img;
     };
