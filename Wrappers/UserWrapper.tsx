14159063fad370ddc280fe89d62390e5e40b50bc
Jan Tichavský <honza@sizefire.com>
2023-01-08 23:42:33 +0100

    userwrapper fetch correctly

--- Wrappers/UserWrapper.tsx
+++ Wrappers/UserWrapper.tsx
@@ -1,25 +1,33 @@
-import { TUser } from "../Const/User";
-import { createSafeContext } from "../basic/createSafeContext";
-import { UserApi } from "../API/UserApi";
-import { useQuery } from "react-query";
-import { Spinner } from "react-utils/Components/Spinner";
+import { TUser } from '../Const/User';
+import { createSafeContext } from '../basic/createSafeContext';
+import { UserApi } from '../API/UserApi';
+import { useQuery } from 'react-query';
+import { Spinner } from 'react-utils/Components/Spinner';
 
-export const [userContext, useUser] = createSafeContext<TUser>("user");
+export const [userContext, useUser] = createSafeContext<TUser>('user');
 
 type Props = {
     children: React.ReactNode;
 };
 
 export const UserWrapper = ({ children }: Props) => {
-    const { isLoading, refetch, data: user } = useQuery<TUser>("loadUser", updateUser);
+    const {
+        isLoading,
+        refetch,
+        data: user,
+    } = useQuery<TUser>('loadUser', updateUser);
 
     window.updateUser = async () => {
         refetch();
     };
 
-    if (isLoading) return <Spinner msg={_("Logging in")} />;
+    if (isLoading) return <Spinner msg={_('Logging in')} />;
 
-    return <userContext.Provider value={user ?? visitorUser}>{children}</userContext.Provider>;
+    return (
+        <userContext.Provider value={user ?? visitorUser}>
+            {children}
+        </userContext.Provider>
+    );
 };
 
 const updateUser = async (): Promise<TUser> => {
@@ -29,40 +37,42 @@ const updateUser = async (): Promise<TUser> => {
 };
 
 const loadUser = async (): Promise<TUser> => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
 
     if (!token) return visitorUser;
 
-    //try {
-    const api = new UserApi();
-    const info = await api.getUserInfo();
+    try {
+        const api = new UserApi();
+        const info = await api.getUserInfo();
 
-    return {
-        id: info.accountId,
-        name: info.accountDetails.name ?? "",
-        roles: info.accountDetails.roles,
-        token,
-        imageUrl: info.accountDetails.image,
-    };
-    /*  } catch {
+        return {
+            id: info.accountId,
+            name: info.accountDetails.name ?? '',
+            roles: info.accountDetails.roles,
+            token,
+            imageUrl: info.accountDetails.image,
+        };
+    } catch {
+        // localStorage.removeItem('token');
+        // return visitorUser;
         return adminUser;
-    }*/
+    }
 };
 
 const visitorUser: TUser = {
     id: 0,
-    name: "Visitor",
-    roles: ["VISITOR"],
-    token: "",
-    imageUrl: "",
+    name: 'Visitor',
+    roles: ['VISITOR'],
+    token: '',
+    imageUrl: '',
 };
 
 const adminUser: TUser = {
     id: 1,
-    name: "Admin",
-    roles: ["ADMIN"],
-    token: "",
-    imageUrl: "",
+    name: 'Admin',
+    roles: ['ADMIN'],
+    token: '',
+    imageUrl: '',
 };
 
 export const currentUser = { user: visitorUser };
