312c5e2b6a85628bfdf9ddb5db3b7c83147d4445
Jan Tichavský <honza@sizefire.com>
2023-04-08 13:42:34 +0200

    make TECH_TREE_NAMES constants

--- Const/Service.ts
+++ Const/Service.ts
@@ -5,6 +5,6 @@ export const TECH_TREE_NAMES = [
     'TEMPLATE_USER',
     'TEMPLATE_DOWNLOAD',
     'TEMPLATE_APPROVE',
-];
+] as const;
 
 export type TTechTreeName = typeof TECH_TREE_NAMES[number];
