eada376afd2a538b6c13e4ec661239b5d4d17414
Jan Tichavský <honza@sizefire.com>
2023-04-16 23:54:00 +0200

    add createPaymentSandbox

--- API/PayUApi.ts
+++ API/PayUApi.ts
@@ -19,6 +19,21 @@ export class PayUApi extends BaseApi {
         return JSON.parse(res).redirectUrl;
     }
 
+    async createPaymentSandbox(setId: number, email: string): Promise<string> {
+        const ip = await this.getIp();
+
+        const data = {
+            setId,
+            customerIp: ip,
+            buyerInfo: {
+                email,
+            },
+        };
+
+        const res = await this.send('POST', '/payments/sandbox', JSON.stringify(data));
+        return JSON.parse(res).redirectUrl;
+    }
+
     private getIp = async (): Promise<string> => {
         const res = await fetch('https://api.ipify.org?format=json');
         return (await res.json()).ip;
