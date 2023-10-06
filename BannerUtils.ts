0d93a72265a02252cdb94225a9b3234179924210
Jan Tichavský <honza@sizefire.com>
2023-10-06 20:21:05 +0200

    getAllExtentsOfMedia

--- BannerUtils.ts
+++ BannerUtils.ts
@@ -1,5 +1,6 @@
 import { BANNERS, BANNER_EXTENTS, TBannerMedia } from './Const/Banners';
 
-export const getAllExtentsOfMedia = (mediaList: TBannerMedia[]) => {
+export const getAllExtentsOfMedia = (mediaList: TBannerMedia[] | 'all') => {
+    if (mediaList === 'all') return BANNER_EXTENTS;
     return BANNER_EXTENTS.filter((extent) => mediaList.includes(BANNERS[extent].media));
 };
