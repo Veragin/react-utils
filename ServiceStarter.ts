3eb76fdf991cd50d1cef9bddc55e45175cd1b43a
Jan Tichavský <honza@sizefire.com>
2024-01-15 20:56:45 +0100

    rename to baseServiceUrl

--- ServiceStarter.ts
+++ ServiceStarter.ts
@@ -2,38 +2,38 @@ class ServiceStarter {
     toHomePage = () => window.open('https://www.sizefire.com', '_self');
 
     createPublicFromScratch = () => {
-        window.open(`${this.baseUrl}?a=service&techTree=TEMPLATE_MAKER`);
+        window.open(`${this.baseServiceUrl}?a=service&techTree=TEMPLATE_MAKER`);
     };
 
     createPublicFromPSD = () => {
-        window.open(`${this.baseUrl}?a=service&techTree=TEMPLATE_RESIZE`);
+        window.open(`${this.baseServiceUrl}?a=service&techTree=TEMPLATE_RESIZE`);
     };
 
     editTemplate = (setId: number) => {
-        window.open(`${this.baseUrl}?a=service&techTree=TEMPLATE_EDIT&setId=${setId}`);
+        window.open(`${this.baseServiceUrl}?a=service&techTree=TEMPLATE_EDIT&setId=${setId}`);
     };
 
     downloadTemplate = (setId: number) => {
-        window.open(`${this.baseUrl}?a=service&techTree=TEMPLATE_DOWNLOAD&setId=${setId}`);
+        window.open(`${this.baseServiceUrl}?a=service&techTree=TEMPLATE_DOWNLOAD&setId=${setId}`);
     };
 
     openDownloadInfo = (setId: number) => {
-        window.open(`${this.baseUrl}?a=info&info=downloadInfo&setId=${setId}`, '_self');
+        window.open(`${this.baseServiceUrl}?a=info&info=downloadInfo&setId=${setId}`, '_self');
     };
 
     openDownloadError = (setId: number) => {
-        window.open(`${this.baseUrl}?a=info&info=downloadError&setId=${setId}`, '_self');
+        window.open(`${this.baseServiceUrl}?a=info&info=downloadError&setId=${setId}`, '_self');
     };
 
     openThankYouDownload = () => {
-        window.open(`${this.baseUrl}/thankyou/downloaded/`, '_self');
+        window.open(`https://sizefire.com/thankyou/downloaded/`, '_self');
     };
 
     openUnknownError = () => {
-        window.open(`${this.baseUrl}?a=info&info=unknownError`, '_self');
+        window.open(`${this.baseServiceUrl}?a=info&info=unknownError`, '_self');
     };
 
-    private get baseUrl() {
+    private get baseServiceUrl() {
         return 'https://sizefire.com/service';
     }
 }
