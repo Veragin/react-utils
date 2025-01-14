4962f27f4753d97da5639ed2a151fc587850b01b
Jan Tichavský <honza@sizefire.com>
2025-01-14 23:10:55 +0100

    RsIconButton color

--- Components/RsIconButton.tsx
+++ Components/RsIconButton.tsx
@@ -4,10 +4,13 @@ type Props = {
     tooltip: string;
     onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     children: React.ReactNode;
+    color?: 'inherit' | 'primary' | 'secondary' | 'default';
 };
 
-export const RsIconButton = ({ tooltip, onClick, children }: Props) => (
+export const RsIconButton = ({ tooltip, onClick, children, color }: Props) => (
     <Tooltip title={tooltip}>
-        <IconButton onClick={onClick}>{children}</IconButton>
+        <IconButton onClick={onClick} color={color}>
+            {children}
+        </IconButton>
     </Tooltip>
 );
